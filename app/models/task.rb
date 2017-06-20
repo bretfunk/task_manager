426b42c5908a9471939a3e4e91bbc18643704f2f
Bret Funk <bret.e.funk@gmail.com>
2017-06-20 18:35:46 -0500

    finished task_manager_app walkthru and refactor

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,21 +1,27 @@
 require 'sqlite3'
 
 class Task
-  attr_reader :title, :description
+  attr_reader :title, :description, :id
   def initialize(task_params)
     @description = task_params["description"]
     @title       = task_params["title"]
     @database = SQLite3::Database.new('db/task_manager_development.db')
     @database.results_as_hash = true
+    @id = task_params["id"] if task_params["id"]
   end
 
   def save
     @database.execute("INSERT INTO tasks (title, description) VALUES (?, ?);", @title, @description)
   end
 
-  def self.all
+  def self.database
     database = SQLite3::Database.new('db/task_manager_development.db')
     database.results_as_hash = true
+    database
+  end
+
+  def self.all
+    database
     tasks = database.execute("SELECT * FROM tasks")
     tasks.map do |task|
       Task.new(task)
@@ -23,8 +29,7 @@ class Task
   end
 
   def self.find(id)
-    database = SQLite3::Database.new('db/task_manager_development.db')
-    database.results_as_hash = true
+    database
     task = database.execute("SELECT * FROM tasks WHERE id = ?", id).first
     Task.new(task)
   end
